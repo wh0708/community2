bd7f31867ccbd5e6811f019b15ded61c9248edf4
wh <296006803@qq.com>
2021-11-24 14:59:15 +0800

    关注、取消关注

---
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -0,0 +1,58 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author WH
+ * @create 2021/11/23 15:44
+ */
+@Controller
+public class FollowController implements CommunityConstant {
+
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/follow", method = RequestMethod.POST)
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.follow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已关注!");
+    }
+
+    @RequestMapping(path = "/unfollow", method = RequestMethod.POST)
+    @ResponseBody
+    public String unfollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.unfollow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已取消关注!");
+    }
+//    private boolean hasFollowed(int userId) {
+//        if (hostHolder.getUser() == null) {
+//            return false;
+//        }
+//
+//        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+//    }
+}
+
